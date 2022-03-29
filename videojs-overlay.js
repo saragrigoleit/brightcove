df95fede1ffbfa446d7feaa6cba3f12295abf858
saragrigoleit <102606392+saragrigoleit@users.noreply.github.com>
2022-03-29 12:32:18 -0500

    Update videojs-overlay.js

--- videojs-overlay.js
+++ videojs-overlay.js
@@ -1,1 +1,10 @@
+<video-js id="myPlayerID"
+    data-account="1752604059001"
+    data-player="a5bb5681-9bfb-4203-9dac-eb3b98c8b4b2"
+    data-embed="default"
+    class="video-js" controls>
+</video-js>
 
+<script src="https://players.brightcove.net/1752604059001/a5bb5681-9bfb-4203-9dac-eb3b98c8b4b2_default/index.min.js"></script>
+
+<script src="https://players.brightcove.net/videojs-overlay/2/videojs-overlay.min.js"></script>
